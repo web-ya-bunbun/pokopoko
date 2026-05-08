557530ead1d4bb3e167133171b220b87c78f9a1b
Saeko Kokubun <pomodoro.e.spinaci@gmail.com>
2026-05-08 15:53:09 +0900

    splide設置

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log('こんにちは');
+if (document.querySelector('.splide')) {
+  new Splide('.splide', {
+    type: 'loop',
+    perPage: 3,
+    breakpoints: {
+      900: {
+        perPage: 2,
+      },
+      480: {
+        perPage: 1,
+      },
+    },
+  }).mount();
+}
