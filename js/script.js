ba7c7319bef0bc8ecdd7cba45f83fb0c9f5667ec
Saeko Kokubun <pomodoro.e.spinaci@gmail.com>
2026-05-08 17:46:44 +0900

    カフェの猫たち部分完成

--- js/script.js
+++ js/script.js
@@ -2,12 +2,17 @@ if (document.querySelector('.splide')) {
   new Splide('.splide', {
     type: 'loop',
     perPage: 3,
+    gap: '2rem',
+    padding: '1rem',
+    pagination: false,
     breakpoints: {
       900: {
         perPage: 2,
+        padding: '2vw',
       },
       480: {
         perPage: 1,
+        padding: '4vw',
       },
     },
   }).mount();
